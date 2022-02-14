ca56182130b3af5bb0fa7e7e746961b8f55d41f6
Seong-uk-An <asu970602@naver.com>
2022-02-14 12:43:13 +0900

    seperate files

---
+++ routes/get.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const { dbService } = require("../fb");
+const { collection, query, getDocs } = require("firebase/firestore");
+
+const app = express();
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+app.set("view engine", "html");
+
+app.get("/", async (req, res) => {
+  const q = query(collection(dbService, "foods"));
+  const querySnapshot = await getDocs(q);
+  let foodArr = [];
+  querySnapshot.forEach((doc) => {
+    const foodObj = {
+      ...doc.data(),
+      id: doc.id,
+    };
+    foodArr.push(foodObj);
+    foodArr.sort(function (a, b) {
+      return b.createdAt - a.createdAt;
+    });
+  });
+  let fileURL = [];
+  let id = [];
+  let menu = [];
+  let place = [];
+  let address = [];
+  let price = [];
+  let saledPrice = [];
+  let deadline = [];
+  let createdAt = [];
+
+  for (let i = 0; i < foodArr.length; i++) {
+    fileURL.push(foodArr[i].fileURL);
+    id.push(foodArr[i].id);
+    menu.push(foodArr[i].menu);
+    place.push(foodArr[i].place);
+    address.push(foodArr[i].address);
+    price.push(foodArr[i].price);
+    saledPrice.push(foodArr[i].saledPrice);
+    deadline.push(foodArr[i].deadline);
+    createdAt.push(foodArr[i].createdAt);
+  }
+
+  res.render("index", {
+    fileURL,
+    id,
+    menu,
+    place,
+    address,
+    price,
+    saledPrice,
+    deadline,
+    createdAt,
+  });
+});
+
+module.exports = app;
---
+++ routes/post.js
@@ -0,0 +1,45 @@
+//npm 모듈 불러오기
+const express = require("express");
+const multer = require("multer");
+
+//기타 모듈 불러오기
+const { dbService, storageService } = require("../fb");
+const { addDoc, collection } = require("firebase/firestore");
+const { getDownloadURL, ref, uploadString } = require("firebase/storage");
+
+//초기세팅
+const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+//index.html로 부터 받은 form
+app.post("/", upload.single("foodImg"), async (req, res) => {
+  try {
+    const foodImg = req.file.buffer.toString("base64");
+    const menu = req.body.menu;
+    const place = req.body.place;
+    const address = req.body.address;
+    const price = req.body.price;
+    const saledPrice = req.body.saledPrice;
+    const deadline = req.body.deadline;
+    const createdAt = Date.now();
+    const fileRef = ref(storageService, `/images/${createdAt}`);
+    const response = await uploadString(fileRef, foodImg, "base64");
+    const fileURL = await getDownloadURL(response.ref);
+    await addDoc(collection(dbService, "foods"), {
+      fileURL,
+      menu,
+      place,
+      address,
+      price,
+      saledPrice,
+      deadline,
+      createdAt,
+    });
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+});
+
+module.exports = app;
