af8ef36b2313639974da1bfc1affb2bca876a060
Seong-uk-An <asu970602@naver.com>
2022-02-14 15:18:00 +0900

    add delete btn

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 
 //기타 모듈 불러오기
+const { dbService, storageService } = require("./fb");
+const { query, doc, deleteDoc } = require("firebase/firestore");
+const { ref, deleteObject } = require("firebase/storage");
+
 //**************** 아래 주석은 삭제 노노!!! ****************
 // const { dbService, storageService } = require("./fb");
 // const {addDoc,collection,query,onSnapshot,orderBy,doc,deleteDoc,getDocs,limit} = require("firebase/firestore");
@@ -26,9 +30,14 @@ app.use(express.static(__dirname + "/public"));
 
 //get
 app.use("/", getRouter);
-app.get("/delete-item", (req, res) => {
-  console.log(req.query);
-  res.send(req.query.id);
+app.get("/delete-item", async (req, res) => {
+  const id = req.query.id;
+  const ca = req.query.ca;
+  const q = query(doc(dbService, "foods", `${id}`));
+  const fileRef = ref(storageService, `/images/${ca}`);
+  await deleteObject(fileRef);
+  await deleteDoc(q);
+  res.redirect("/");
 });
 
 //post
